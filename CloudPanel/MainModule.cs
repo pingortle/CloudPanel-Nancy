9cbc851dd87d532007fff7f538f457551141a0f0
Kaleb Lape <pingortle@gmail.com>
2014-04-22 09:22:49 -0500

    Initial commit.

---
+++ CloudPanel/MainModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+
+namespace CloudPanel
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => "Welcome to CloudPanel!";
+
+            Get["/login"] = _ => "Username: ________\n Password: ________";
+
+            Get["/dashboard"] = _ => "This is the dashboard.";
+        }
+    }
+}
