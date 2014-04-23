e6a0625772e08bd1c138c786a32916b7c5f31538
Kaleb Lape <pingortle@gmail.com>
2014-04-23 17:25:30 -0500

    First stab at settings.

---
+++ CloudPanel/Modules/AuthRoot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+using Nancy.Security;
+
+namespace CloudPanel.Modules
+{
+    public class AuthRoot : NancyModule
+    {
+        public AuthRoot()
+        {
+            this.RequiresAuthentication();
+
+            Get["/settings"] = _ => View["settings"];
+        }
+    }
+}
